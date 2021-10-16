e66ce25b0c24965a91d4c90c46ef6947ba436a74
Brijesh Gupta <brijesh.gupta@datamatics.com>
2021-10-16 17:46:39 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Assignement_Crawford.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
